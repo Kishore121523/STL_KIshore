b06c571a44806422f6ef908cabcb522c3b64a207
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-11-06 23:33:57 -0500

    Added Priority Queue

---
+++ STL_FOLDER/PriorityQueue/STL_PQ.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+  // The element which has the largest value stays at the top
+  // Not a linear data structure, inside this a tree data structure is maintained
+
+  priority_queue<int> pq;
+  pq.push(5); // {5}
+  pq.push(2); // {5,2}
+  pq.push(8); // {8,5,2}
+  pq.push(10); // {10,8,5,2}
+
+  cout << pq.top() << " "; // 10
+
+  pq.pop();
+
+  cout << pq.top() << " "; // 8
+
+  // Minimum heap - The element which has the lowest values stays at the top 
+  priority_queue<int, vector<int>, greater<int>> mpq;
+  mpq.push(5); // {5}
+  mpq.push(2); // {2,5}
+  mpq.push(8); // {2,5,8}
+  mpq.push(10); // {2,5,8,10}
+
+  cout << mpq.top() << " "; // 2
+  return 0;
+}
