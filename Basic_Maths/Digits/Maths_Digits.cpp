b4cf46a94da5a4728a0b48fa2d4496a8765a7977
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-11-09 01:02:27 -0500

    Added Digit Extraction

---
+++ Basic_Maths/Digits/Maths_Digits.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void extractDigits(int n){
+  while(n>0){
+    cout << n%10 << " "; // 54321%10 = 1
+    n = n/10; // n = 54321/10 = 5432
+  }
+}
+
+int countingNumberOfDigits(int n){
+  int count = 0;
+  while(n>0){
+    count += 1;
+    n = n/10;
+  }
+  return count;
+}
+
+int countingNumberOfDigitsUsingLog(int n){
+  int count = log10(n) + 1; // log10(54321) = will return a number starting with 4, so add 1
+  return count;
+}
+
+int main() {
+
+  // Extracting all the digits from a given number N, the digits will be extracted in reverse order
+  extractDigits(54321);
+  cout<<'\n';
+  cout << countingNumberOfDigits(54321) << "\n";
+  cout << countingNumberOfDigitsUsingLog(54321) << "\n";
+
+  return 0;
+}
