5f3c4dad2ee16ed2592f4c37a18660aa57d905e5
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-11-18 01:38:24 -0500

    Edited  Hashing-Number

--- Basic_Maths/Hashing/Number_Hashing.cpp
+++ Basic_Maths/Hashing/Number_Hashing.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main(){
   // pre-storing and fetching if needed - hashing
   int n = 5;
-  int arr[5] = {1,3,2,1,3}; // input array which can have values between 1 to 12
+  vector<int> arr = {1,3,2,1,3}; // input array which can have values between 1 to 12
 
   // precompute
   int hash[13] = {0}; // declaring the hash array and initialising the count with 0
-  for(int i=0; i<n; i++){
+  for(int i=0; i<arr.size(); i++){
     hash[arr[i]] += 1;
   }
 
