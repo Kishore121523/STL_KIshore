ea3987593deaf77a17be243870ffe8a108c92380
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-11-21 13:45:47 -0500

    Edited Selection Sort

--- Sorting/SelectionSort.cpp
+++ Sorting/SelectionSort.cpp
@@ -10,9 +10,10 @@ int main(){
 
   // 13,46,24,52,20,9 - Input 
   // 9,46,24,52,20,13 - Swap 0th index with min element [0 to n-1 array]
-  // 9,13,24,52,20,46 - Step 1st index with min element [1 to n-1 array]
-  // 9,13,20,52,24,46 - Step 2nd index with min element [2 to n-1 array]
-  // 9,13,20,24,52,46 - Step 3rd index with min element [3 to n-1 array]
+  // 9,13,24,52,20,46 - Swap 1st index with min element [1 to n-1 array]
+  // 9,13,20,52,24,46 - Swap 2nd index with min element [2 to n-1 array]
+  // 9,13,20,24,52,46 - Swap 3rd index with min element [3 to n-1 array]
+  // 9,13,20,24,46,52 - Swap 4th index with min element [4 to n-1 array]
   // 9,13,20,24,46,52 - Sorted
   // 6 elements, sorted in 5 stepes - n-1 steps where n is number of elements in input array
 
