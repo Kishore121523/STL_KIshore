70c8fe56a9439d4e240bd2b09744c985bfe5a544
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-11-20 23:09:01 -0500

    Edited Selection Sort

--- Sorting/SelectionSort.cpp
+++ Sorting/SelectionSort.cpp
@@ -3,17 +3,38 @@
 using namespace std;
 
 int main(){
-  // Selection sort - we select minimums
+  // Selection sort - we select minimums and swap
   // Step 1 - get the minimum element in the array and swap it with the first element
   // Step 2 - From second element, find minimum of the array and swap it with second element
   // Step 3 - From the third element, repeat the same steps till end of the array
 
   // 13,46,24,52,20,9 - Input 
-  // 9,46,24,52,20,13 - Step 1
-  // 9,13,24,52,20,46 - Step 2
-  // 9,13,20,52,24,46 - Step 3
-  // 9,13,20,24,52,46 - Step 4
-  // 9,13,20,24,46,52 - Step 5 - sorted
+  // 9,46,24,52,20,13 - Swap 0th index with min element [0 to n-1 array]
+  // 9,13,24,52,20,46 - Step 1st index with min element [1 to n-1 array]
+  // 9,13,20,52,24,46 - Step 2nd index with min element [2 to n-1 array]
+  // 9,13,20,24,52,46 - Step 3rd index with min element [3 to n-1 array]
+  // 9,13,20,24,46,52 - Sorted
   // 6 elements, sorted in 5 stepes - n-1 steps where n is number of elements in input array
+
+  vector<int> arr = {13,46,24,52,20,9};
+  int n = arr.size();
+
+  for (int i=0; i<=n-2; i++){
+    int min = i;
+
+    for(int j=i; j<=n-1; j++){
+      if(arr[j] < arr[min]){
+        min = j;
+      }
+    }
+    // swap
+    int temp = arr[min];
+    arr[min] = arr[i];
+    arr[i] = temp;
+  }
+
+  for(auto it: arr){
+    cout << it << ", ";
+  }
   return 0;
 }
