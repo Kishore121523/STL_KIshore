4530d7a3fc63f52f8ddda064fc9704d0e156dbeb
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-11-24 11:59:10 -0500

    Edited Bubble Sort

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -45,6 +45,7 @@ int main(){
   for(int i=0; i<n-1; i++){
     for(int j=0; j<n-i-1; j++){
       if(arr[j] > arr[j+1]){
+        // swapping operation using temp variable
         int temp = arr[j];
         arr[j] = arr[j+1];
         arr[j+1] = temp;
