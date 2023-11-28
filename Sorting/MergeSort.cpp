dfa439e5a0e20f7a7ba49566b346156c927f2c63
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-11-27 22:08:35 -0500

    Edited MergeSort

--- Sorting/MergeSort.cpp
+++ Sorting/MergeSort.cpp
@@ -53,6 +53,7 @@ int main(){
   // This algorithm hypothetically divides the array into 2 parts
   // Then, left part is divided into 2 parts and right part into two parts. And recursively repeats the same process until the elements in the divided parts can't be divided further.
   // Later, the divided arrays are merged. While merging, the two arrays will be sorted and merged.
+  // Time complexity - O(N * log2(N)) and Space Complexity - O(N)
   vector<int> arr = {3,2,4,1,3};
   int n = arr.size();
 
