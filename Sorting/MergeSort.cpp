a73ca49d62f9d161afd0a59f1697384bf6fbf77c
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-11-27 21:28:38 -0500

    Added MergeSort

---
+++ Sorting/MergeSort.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  // Focusses on Divide and Conquer Method
+  // This algorithm hypothetically divides the array into 2 parts
+  // Then, left part is divided into 2 parts and right part into two parts. And recursively repeats the same process until the elements in the divided parts can't be divided further.
+  // Later, the divided arrays are merged. While merging, the two arrays will be sorted and merged.
+  
+  
+  return 0;
+}
