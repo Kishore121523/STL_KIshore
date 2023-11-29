93241a3537c73ae009cd06485e44293c7a51c002
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-11-28 23:25:23 -0500

    Edited Largest Element in array

--- Arrays_Problems/Easy/LargestElement.cpp
+++ Arrays_Problems/Easy/LargestElement.cpp
@@ -2,6 +2,21 @@
 
 using namespace std;
 
+int largestElement(vector<int> &arr, int n){
+  int largest = arr[0];
+
+  for(int i=0; i<n; i++){
+    if(arr[i]>=largest) {
+      largest = arr[i];
+    }
+  }
+
+  return largest;
+}
 int main(){
+  vector<int> arr = {5,65,3,234,5,67};
+  int n = arr.size();
+
+  cout<<largestElement(arr,n);
   return 0;
 }
