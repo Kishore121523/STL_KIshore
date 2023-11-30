713362a0e7dbb1ab56249e805e83c08d713a77d7
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-11-29 22:53:50 -0500

    Added Second largest element in array

--- Arrays_Problems/Easy/LargestElement.cpp
+++ Arrays_Problems/Easy/LargestElement.cpp
@@ -13,10 +13,30 @@ int largestElement(vector<int> &arr, int n){
 
   return largest;
 }
+
+int secondLargestElement(vector<int> &arr, int n){
+  int largest = arr[0];
+  int secondLargest = INT_MIN;
+
+  for(int i=1; i<n; i++){
+    if(arr[i]>largest){
+      secondLargest = largest;
+      largest = arr[i];
+    }
+    else if(arr[i] < largest && arr[i] > secondLargest){
+      secondLargest = arr[i];
+    }
+  }
+
+  return secondLargest;
+}
+
 int main(){
   vector<int> arr = {5,65,3,234,5,67};
   int n = arr.size();
 
   cout<<largestElement(arr,n);
+  cout << "\n";
+  cout <<secondLargestElement(arr,n);
   return 0;
 }
