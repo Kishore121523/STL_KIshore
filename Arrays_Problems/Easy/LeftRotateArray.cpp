2961648b6016a263c2508cafc5f69698262c6986
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-12-01 23:29:39 -0500

    Editied LeftRotateArray

--- Arrays_Problems/Easy/LeftRotateArray.cpp
+++ Arrays_Problems/Easy/LeftRotateArray.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+void leftRotateArrayBy1Place(vector<int> &arr, int n){
+  int temp = arr[0];
+
+  for(int i=1; i<n; i++){
+    arr[i-1] = arr[i];
+  }
+  arr[n-1] = temp;
+}
+
 int main(){
+  vector<int> arr = {1,2,3,4,5};
+  int n = arr.size();
+
+  leftRotateArrayBy1Place(arr, n); // O(n) time complexity - optimal solution
+  for(auto it: arr){
+    cout<<it<<", ";
+  }
   return 0;
 }
