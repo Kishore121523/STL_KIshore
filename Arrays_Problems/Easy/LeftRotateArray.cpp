a167c9c647fba2e8561dda70bc7a71ffdb6a4c49
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-12-02 00:03:04 -0500

    Editied LeftRotateArray

--- Arrays_Problems/Easy/LeftRotateArray.cpp
+++ Arrays_Problems/Easy/LeftRotateArray.cpp
@@ -11,8 +11,18 @@ void leftRotateArrayBy1Place(vector<int> &arr, int n){
   arr[n-1] = temp;
 }
 
+void reverseFoo(vector<int> &arr, int start, int end){
+  while(start<=end){
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+}
+
 void leftRoateArraybyDPlaces(vector<int> &arr, int n, int d){
-  d = d%n; // Ex: 20 rotations in 7(d) element array - After 7 rotation the array is original array. Hence, 7+7+6 = 20 roation. Hence, only 6 rotation is enough for doing 20 rotation
+  // d = d%n; // Ex: 20 rotations in 7(d) element array - After 7 rotation the array is original array. Hence, 7+7+6 = 20 roation. Hence, only 6 rotation is enough for doing 20 rotation
 
   // // Brute approach with extra temp array
   // int temp[d];
@@ -25,6 +35,11 @@ void leftRoateArraybyDPlaces(vector<int> &arr, int n, int d){
   // for(int i = n-d; i<n; i++){
   //   arr[i] = temp[i-(n-d)];
   // }
+
+  // Space optimal approach, without using temp array
+  reverseFoo(arr,0,d-1); // reverse 0,1,2 index - first 3 elements since d is 3
+  reverseFoo(arr,d,n-1); // reverse 3,4 index = last two elements
+  reverseFoo(arr,0,n-1); // reverse entire array to get the result
 
    
 }
@@ -41,7 +56,7 @@ int main(){
 
   cout <<"\n";
 
-  leftRoateArraybyDPlaces(arr2,n,2);
+  leftRoateArraybyDPlaces(arr2, n, 3);
    for(auto it: arr2){
     cout<<it<<", ";
   }
