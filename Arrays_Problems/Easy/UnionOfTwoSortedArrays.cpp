b44ecbd07b19680b988a82603d6dcf03ea80c0d0
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-12-04 21:55:29 -0500

    Added UnionOfTwoSortedArrays

---
+++ Arrays_Problems/Easy/UnionOfTwoSortedArrays.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+set<int> findUnion(vector<int> &arr1, vector<int> &arr2){
+  set<int> st;
+  int n1 = arr1.size();
+  int n2 = arr2.size();
+
+  for(int i=0; i<n1; i++){
+    st.insert(arr1[i]);
+  }
+  for(int i=0; i<n1; i++){
+    st.insert(arr2[i]);
+  }
+
+  return st;
+
+}
+
+vector<int> findUnionOptimal(vector<int> &arr1, vector<int> &arr2){
+  int i = 0;
+  int j = 0;
+  int n1 = arr1.size();
+  int n2 = arr2.size();
+  vector<int> unionArr;
+
+  while(i<n1 && j<n2){
+    if(arr1[i] <= arr2[j]){
+      if(unionArr.size()==0 || unionArr.back() != arr1[i]){
+          unionArr.push_back(arr1[i]);
+      }
+      i++;
+    }
+    else{
+      if(unionArr.size()==0 || unionArr.back() != arr2[j]){
+          unionArr.push_back(arr2[j]);
+      }
+      j++;
+    }
+  }
+  
+  // When arr1 comes to end, copy all the items in the arr2 to union array
+  while(j<n2){
+    if(unionArr.size()==0 || unionArr.back() != arr2[j]){
+          unionArr.push_back(arr2[j]);
+      }
+      j++;
+  }
+
+  // When arr2 comes to end, copy all the items in the arr1 to union array
+  while(i<n1){
+    if(unionArr.size()==0 || unionArr.back() != arr1[i]){
+          unionArr.push_back(arr1[i]);
+      }
+      i++;
+  }
+
+  return unionArr;
+}
+
+int main(){
+  vector<int> arr1 = {1,1,2,3,4,5};
+  vector<int> arr2 = {2,3,4,4,5,6};
+  // Union of two arrays above is [1,2,3,4,5,6]
+
+  set<int> st = findUnion(arr1, arr2);
+
+  for(auto it: st){
+    cout << it << ", ";
+  }
+
+  cout << "\n";
+
+  vector<int> unionArr;
+  unionArr = findUnionOptimal(arr1,arr2);
+
+  for(auto it: unionArr){
+    cout << it << ", ";
+  }
+  return 0;
+}
