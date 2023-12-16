979a72675c0a902bb4b6a060a51f2cef65c740b1
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-12-16 13:58:39 -0500

    Added LongestSubArrayWithSumK

---
+++ Arrays_Problems/Easy/LongestSubArrayWithSumK.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int longestSubArrBrute(vector<int> &arr, int n, int k){
+  int len = 0;
+  for(int i =0 ;i<n; i++){
+    for(int j=i;j<n;j++){
+        int sum = 0;
+        for(int k=i; k<=j; k++){
+          sum += arr[k];
+        } 
+      if(sum == k) len = max(len, j-i+1);
+    }
+  }
+  return len;
+}
+
+int longestSubArrOptimal(vector<int> &arr, int n, int k){
+
+}
+
+int main(){
+  // Given sum K, return the length of subarray which is longest with sum K
+  vector<int> arr = {1,2,3,1,1,1,4,2,3};
+  int n = arr.size();
+  int k = 3;
+
+  cout << longestSubArrBrute(arr,n,k)<<"\n";
+  cout << longestSubArrOptimal(arr,n,k)<<"\n";
+
+  return 0; 
+}
