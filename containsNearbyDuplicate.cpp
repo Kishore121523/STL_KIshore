27e079e47bf94f549d31b6423e1a660fe4ee7136
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-12-19 15:35:51 -0500

    Added containsNearbyDuplicate

---
+++ containsNearbyDuplicate.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+  bool bool1(vector<int>& nums, int k) {
+       unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++) {
+            if(mp.count(nums[i]))
+            {
+                int diff = abs(mp[nums[i]] - i);
+                if (diff <= k)return true;  
+            }
+            mp[nums[i]] = i;
+        }
+        return false;
+        
+}
+
+int main(){
+  // Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+  vector<int> nums = {1,0,1,1};
+  int k = 1;
+
+  cout << bool1(nums, k);
+  return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,5 +1,2 @@
- // for(int i=0; i<hashArr.size(); i++){
-        //     if(hashArr[i] > 1) return false;
-        // }
-
-        // return true;
+cout << it->first << ", " << it->second;
+          // cout << "\n";
