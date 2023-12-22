bd707e4c51a7dc4bc03990148cfaac688e9bdb8f
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-12-22 00:14:29 -0500

    Edited LongestSubArrayWithSumK

--- Arrays_Problems/Easy/LongestSubArrayWithSumK.cpp
+++ Arrays_Problems/Easy/LongestSubArrayWithSumK.cpp
@@ -16,24 +16,51 @@ int longestSubArrBrute(vector<int> &arr, int n, int k){
   return len;
 }
 
-int longestSubArrOptimal(vector<int> &arr, int n, long long k){
+int longestSubArrBetter(vector<int> &arr, int n, long long k){
   map<long long, int> prefSumMap;
   long long sum = 0;
   int maxLen = 0;
 
   for(int i=0; i<arr.size(); i++){
     sum += arr[i];
+
     if(sum == k) {
       maxLen = max(maxLen, i+1);
     }
 
     long long remaining = sum - k;
+
     if(prefSumMap.find(remaining) != prefSumMap.end()){
       int len = i - prefSumMap[remaining];
       maxLen = max(maxLen, len);
     }
+
     if(prefSumMap.find(sum) != prefSumMap.end()){
       prefSumMap[sum] = i;
+    }
+  }
+  return maxLen;
+}
+
+int longestSubArrOptimal(vector<int> &arr, int n, long long k){
+  int left = 0, right = 0;
+  long long sum = arr[0];
+  int maxLen = 0;
+
+  while(right<n){
+
+    while(left <= right && sum > k){
+      sum -= arr[left];
+      left++;
+    }
+
+    if(sum == k){
+      maxLen = max(maxLen, right - left + 1 );
+    }
+
+    right++;  
+    if(right<n){
+      sum += arr[right];
     }
   }
   return maxLen;
@@ -46,6 +73,7 @@ int main(){
   int k = 3;
 
   cout << longestSubArrBrute(arr,n,k)<<"\n";
+  cout << longestSubArrBetter(arr,n,k)<<"\n";
   cout << longestSubArrOptimal(arr,n,k)<<"\n";
 
   return 0; 
