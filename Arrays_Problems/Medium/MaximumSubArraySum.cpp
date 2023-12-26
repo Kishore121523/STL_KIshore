bdda1820c16331101572a8b4bf1c0b51ea9b30d7
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-12-25 20:52:30 -0500

    Edited MaximumSubArraySum

--- Arrays_Problems/Medium/MaximumSubArraySum.cpp
+++ Arrays_Problems/Medium/MaximumSubArraySum.cpp
@@ -10,7 +10,10 @@ int maxSubarraySum(vector<int> &arr){
   for(int i=0; i<arr.size(); i++){
 
     sum += arr[i];
-    maxi = max(sum,maxi);
+
+    if(sum > maxi){
+      maxi = sum;
+    }
 
     if(sum < 0) sum = 0;
 
@@ -18,9 +21,39 @@ int maxSubarraySum(vector<int> &arr){
     return maxi;
 }
 
+void printmaxSubarraySum(vector<int> &arr){
+  int maxi = INT_MIN;
+  int sum = 0;
+  int startIndex = -1;
+  int endIndex = -1;
+
+  for(int i=0; i<arr.size(); i++){
+    if(sum == 0){
+      startIndex = i;
+    }
+
+    sum += arr[i];
+
+    if(sum > maxi){
+      maxi = sum;
+      endIndex = i;
+    }
+
+    if(sum < 0){
+      sum = 0;
+    }
+  }
+
+  for(int i = startIndex; i<=endIndex; i++){
+    cout << arr[i]<<", ";
+  }
+}
+
 int main(){
   vector<int> arr = {-2,-3,4,-1,-2,1,5,-3};
 
   cout << maxSubarraySum(arr);
+  cout << "\n";
+  printmaxSubarraySum(arr);
   return 0;
 }
