f095780a480dcc83d17395914972049e714a92f6
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-01-02 20:55:03 -0500

    Added SetMatrixZeroes

--- Arrays_Problems/Medium/SetMatrixZeroes.cpp
+++ Arrays_Problems/Medium/SetMatrixZeroes.cpp
@@ -36,18 +36,62 @@ void setMatrixToZeroBetter(vector<vector<int>> &matrix){
 }
 
 void setMatrixToZeroOptimal(vector<vector<int>> &matrix){
+  // To move across row 0 = matrix[0][...]
+  // To move across col 0 = matrix[...][0]
 
   int noOfRows = matrix.size(); // no of columns
   int noOfCols = matrix[0].size(); // no of rows
 
+  int col0 = 1;
 
+  for(int i=0; i<noOfRows; i++){
+    for(int j=0; j<noOfCols; j++){
+      if(matrix[i][j] == 0){
+
+        // marking 0 in the corresponding ith row
+        matrix[i][0] = 0;
+
+        if(j!=0){
+          // marking 0 in the corresponding jth column if its not the first column (j=0)
+          matrix[0][j] = 0;
+        }else{
+          col0 = 0;
+        }
+      }
+    }
+  }
+
+  for(int i=1;i<noOfRows;i++){
+    for(int j=1; j<noOfCols; j++){
+      if(matrix[i][j] != 0){
+        // check for corresponding 0th column and row which for the elements that are remaining
+        if(matrix[0][j] == 0 || matrix[i][0] == 0){
+          matrix[i][j] = 0; // set the element to 0 if its 1 and its corresponding 0th col or row is set to 0
+        }
+      }
+    }
+  }
+
+  // if first element of the matrix is 0, then the entire first row will be 0
+  if(matrix[0][0] == 0){
+    for(int j=0; j<noOfCols; j++){
+      matrix[0][j] = 0;
+    }
+  }
+
+  // if the col0 is 0, then the entire first col will be 0
+  if(col0 == 0){
+    for(int i=0; i<noOfRows; i++){
+      matrix[i][0] = 0;
+    }
+  }
 
   for(int i=0; i<noOfRows; i++){
       for(int j=0; j<noOfCols; j++){
           cout << matrix[i][j];
       }
       cout << "\n";
-    }
+  }
 }
 
 int main(){
