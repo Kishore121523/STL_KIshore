d482439edf2b52a58a02cfa61cad15f3b44cf8de
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-01-05 01:17:21 -0500

    Edited Lower Bound using Binary Search

---
+++ BinarySearch/BinarySearch 1D Arrays/LowerBoundAndUpperBound.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int lowerBound(vector<int> &arr, int n, int target){
+  // int lb = lower_bound(arr.begin(), arr.end(), target) - arr.begin() - STL function
+  // smallest index such that arr[ind] >= target
+  int low = 0;
+  int high = n-1;
+  int ans = n; // initial when the answer is not in array we have to return n
+
+  while(low<=high){
+    int mid = (low+high)/2;
+    if(arr[mid] >= target){
+      ans = mid; // may be an answer, hence move further to the left to find the smallest index
+      high = mid-1;
+    }else{
+      low = mid+1; // can't be an answer hence go the right
+    }
+  }
+    return ans;
+}
+
+int upperBound(vector<int> &arr, int n, int target){
+  // int lb = upper_bound(arr.begin(), arr.end(), target) - arr.begin() - STL function
+  // smallest index such that arr[ind] > target
+  int low = 0;
+  int high = n-1;
+  int ans = n; // initial when the answer is not in array we have to return n
+
+  while(low<=high){
+    int mid = (low+high)/2;
+    if(arr[mid] > target){
+      ans = mid; // may be an answer, hence move further to the left to find the smallest index
+      high = mid-1;
+    }else{
+      low = mid+1; // can't be an answer hence go the right
+    }
+  }
+    return ans;
+}
+
+int searchInsertPosition(vector<int> &arr, int n, int target){
+  return lower_bound(arr.begin(),arr.end(), target) - arr.begin();
+}
+
+
+int main(){
+  vector<int> arr = {1,2,4,5,6,22,25,34};
+  int n = arr.size();
+  int targetElement = 22;
+
+  cout << lowerBound(arr,n,targetElement); // TC = O(log2N)
+  cout << upperBound(arr,n,targetElement); // TC = O(log2N)
+
+  cout << searchInsertPosition(arr,n,targetElement);
+
+  return 0;
+}
