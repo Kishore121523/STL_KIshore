68398a2536f0e0c1fbe805b86a8d272ad74f6eba
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-01-04 22:54:06 -0500

    Added NumberOfSubarraysWithSumK

--- Arrays_Problems/Medium/NumberOfSubArrayWithSumK.cpp
+++ Arrays_Problems/Medium/NumberOfSubArrayWithSumK.cpp
@@ -21,8 +21,25 @@ int numberOfSubArrayWithSumKBetter(vector<int> &arr, int n, int targetSum){
   return cnt;
 }
 
-bool numberOfSubArrayWithSumKOptimal(vector<int> &arr, int n, int targetSum){
-  
+bool numberOfSubArrayWithSumKOptimal(vector<int> &arr, int n, int k){
+  // using prefix sum
+  unordered_map<int,int> mpp;
+  int cnt = 0;
+  int preSum = 0;
+  mpp[preSum] = 1;
+
+  for(int i = 0; i<n; i++){
+    preSum += arr[i];
+    int numberFind = preSum - k;
+
+    if(mpp.find(numberFind) != mpp.end()){
+      cnt += mpp[numberFind];
+    }
+
+    mpp[preSum]++;
+  }
+
+  return cnt;
 }
 
 int main(){
@@ -32,6 +49,6 @@ int main(){
 
   cout << numberOfSubArrayWithSumKBetter(arr,n,targetSum); // TC = O(n^2)
   cout << "\n";
-  cout << numberOfSubArrayWithSumKOptimal(arr,n,targetSum); // TC = O(n^2)
+  cout << numberOfSubArrayWithSumKOptimal(arr,n,targetSum); // TC = O(N*logN) - Normal map and O(N*1) for unordered map
   return 0;
 }
