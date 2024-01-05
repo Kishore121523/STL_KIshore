bc69b51901c78b1601d1db939294d15f81288977
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-01-04 22:42:13 -0500

    Added NumberOfSubarraysWithSumK

---
+++ Arrays_Problems/Medium/NumberOfSubArrayWithSumK.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int numberOfSubArrayWithSumKBetter(vector<int> &arr, int n, int targetSum){
+  int cnt = 0;
+
+  for(int i=0; i<n; i++){
+    int sum = arr[i];
+    if(sum == targetSum){
+      cnt+=1;
+    }
+    for(int j=i+1; j<n; j++){
+      sum += arr[j];
+      if(sum == targetSum) {
+          cnt+=1;
+      }
+    }
+  }
+
+  return cnt;
+}
+
+bool numberOfSubArrayWithSumKOptimal(vector<int> &arr, int n, int targetSum){
+  
+}
+
+int main(){
+  vector<int> arr = {1,2,3,-3,1,1,1,4,2,-3};
+  int n = arr.size();
+  int targetSum = 3;
+
+  cout << numberOfSubArrayWithSumKBetter(arr,n,targetSum); // TC = O(n^2)
+  cout << "\n";
+  cout << numberOfSubArrayWithSumKOptimal(arr,n,targetSum); // TC = O(n^2)
+  return 0;
+}
