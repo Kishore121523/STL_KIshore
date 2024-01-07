5f3248fbbc9c807ee0ff9420ffad0269200764a4
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-01-07 18:40:57 -0500

    Added findMinimumInRotatedSortedArray

---
+++ BinarySearch/BinarySearch 1D Arrays/findMinimumInRotatedSortedArray.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int findMinimumInRotatedSortedArrayElements(vector<int> &arr, int n){
+  int low = 0;
+  int high = n-1;
+  int minElement = INT_MAX;
+
+  while(low<=high){
+    int mid = low + (high-low)/2;
+
+    // Left half is sorted, hence take the smallest and elimate the left half
+    if(arr[low] <= arr[mid]){
+      minElement = min(arr[low], minElement);
+      low = mid + 1;
+    }
+    // Right Half is sorted, hence take the smallest and elimate the right half
+    else{
+      minElement = min(arr[mid], minElement);
+      high = mid - 1;
+    }
+  }
+
+  return minElement;
+}
+
+
+int main(){
+  vector<int> arr = {4,5,6,7,0,1,2};
+  int n = arr.size();
+
+  cout << findMinimumInRotatedSortedArrayElements(arr,n); // TC = O(log2N)
+  cout << "\n";
+
+  return 0;
+}
