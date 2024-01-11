e42aed130bae52fd27826fbf859a94edf37cc3f5
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-01-11 00:19:17 -0500

    Added SplitArrayLargestSum

---
+++ BinarySearch/BinarySearch on Answers/SplitArrayLargestSum.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int countPaintersFoo(vector<int> &arr, int sumPossible){
+  int painterCount = 1;
+  int bricksInTheWall = 0;
+
+  for(int i=0 ;i<arr.size(); i++){
+    if(bricksInTheWall+arr[i] <= sumPossible){
+      bricksInTheWall += arr[i];
+    }else{
+      painterCount++;
+      bricksInTheWall = arr[i];
+    }
+  }
+
+  return painterCount;
+}
+
+int painterPartition(vector<int> &arr, int n, int k){
+  int low = *max_element(arr.begin(), arr.end());
+  int high = accumulate(arr.begin(), arr.end(), 0);
+
+  while(low<=high){
+    int mid = low+ (high-low)/2;
+
+    int countPainters = countPaintersFoo(arr, mid);
+
+    if(countPainters > k){
+      low = mid + 1;
+    }else{
+      high = mid -1 ;
+    }
+  }
+  return low;
+
+}
+
+
+int main(){
+  // Given an integer array nums and an integer k, split nums into k non-empty subarrays such that the largest sum of any subarray is minimized. Return the minimized largest sum of the split.
+  vector<int> arr = {10,20,30,40};
+  int n = arr.size();
+  int k = 2;
+
+  cout << painterPartition(arr,n,k); // TC = O([log2(max-min)*O(N)])
+  cout << "\n";
+
+  return 0;
+}
