739de73042ec434bd464177efdd7882cdb6ee2c9
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-01-18 23:47:54 -0500

    Edited InsertionOfNodeInLL

--- LinkedList/InsertionOfNodeinLL.cpp
+++ LinkedList/InsertionOfNodeinLL.cpp
@@ -52,17 +52,53 @@ Node* insertInHead(Node* head, int val){
 
 Node* insertInTail(Node* head, int val){
   if(head == NULL) return new Node(val);
-  Node* temp = head;
 
+  Node* temp = head;
   //Stop at last index
   while(temp->next != NULL){
       temp = temp->next;
   }
+
   Node* newLast = new Node(val);
   temp->next = newLast;
 
   return head;
 }
+
+Node* insertAtK(Node* head, int k, int val3){
+  if(head==NULL){
+    if(k==1){ 
+      Node* newHead = new Node(val3);
+      return newHead;
+    }else{
+      cout << "Not possible insert element";
+    }
+  }
+
+  if(k==1){
+    Node* newHead = new Node(val3);
+    newHead->next = head;
+    return newHead;
+  }
+
+  Node* temp = head;
+  int cnt = 0;
+
+  while(temp != NULL){
+    cnt++;
+    if(cnt == k-1){
+      Node* newNode = new Node(val3);
+      newNode->next = temp->next;
+      temp->next = newNode;
+      break;
+    }
+    temp = temp->next;
+  }
+
+  return head;
+}
+
+
 int main(){
   vector<int> arr = {2,5,6,7};
   Node* head = convertToLL(arr);
@@ -75,10 +111,17 @@ int main(){
   head = insertInHead(head, val1);
   travereLL(head);
 
-  // Inserting a new node in head
+  // Inserting a new node to tail
   int val2 = 15;
   head = insertInTail(head, val2);
   travereLL(head);
+
+  // Inserting a new node at kth position
+  int k = 4, val3 = 22;
+  head = insertAtK(head, k, val3);
+  travereLL(head);
+
+
 
 
 
