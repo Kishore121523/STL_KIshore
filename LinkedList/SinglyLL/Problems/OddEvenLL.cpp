502b7f06fa00d484585e5bed7a9cc958c3510f6b
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-01-24 23:19:53 -0500

    Added OddEvenLL.cpp

---
+++ LinkedList/SinglyLL/Problems/OddEvenLL.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node{
+  public:
+    int data;
+    Node* next;
+
+  public:
+    Node(int data1, Node* next1){
+      data = data1;
+      next = next1;
+    }
+
+  public:
+    Node(int data1){
+      data = data1;
+      next = nullptr;
+    }
+};
+
+Node* convertToLL(vector<int> &arr){
+  Node* head = new Node(arr[0]);
+  Node* mover = head;
+
+  for(int i=1; i<arr.size(); i++){
+    Node* newNode = new Node(arr[i]);
+    mover->next = newNode;
+    mover = newNode;
+  }
+
+  return head;
+}
+
+Node* oddEvenNaive(Node* head){
+
+  return head;
+  
+}
+
+Node* oddEvenOptimal(Node* head){
+
+  return head;
+  
+}
+
+void traverseLL(Node* head){
+  Node* temp = head;
+
+  while(temp){
+    cout << temp->data << " ";
+    temp = temp->next;
+  }
+}
+
+
+int main(){
+  vector<int> arr = {2,5,6,7,9,10};
+  Node* head = convertToLL(arr);
+  Node* head2 = convertToLL(arr);
+
+  head = oddEvenNaive(head); // O(2N)
+  traverseLL(head);
+
+  cout << "\n";
+
+  head2 = oddEvenOptimal(head2); // O(N)
+  traverseLL(head2);
+
+  return 0;
+
+}
