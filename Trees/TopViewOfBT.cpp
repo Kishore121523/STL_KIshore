ec090bfb8922031f96fc11251caa727d4d196aea
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-02-15 00:01:56 -0500

    Edited TopViewOfBT

--- Trees/TopViewOfBT.cpp
+++ Trees/TopViewOfBT.cpp
@@ -37,9 +37,19 @@ vector<int> topView(Node* root){
     int line = it.second;
 
     // pushing it into the map if the line is not available before in the map
+    if(mpp.find(line) == mpp.end()) mpp[line] = node->data;
     
+    // if left/right node is present push it to the queue
+    if(node->left!=NULL){
+      q.push({node->left, line-1});
+    }
+    if(node->right!=NULL){
+      q.push({node->right, line+1});
+    }
+  }
 
-
+  for(auto it: mpp){
+    ans.push_back(it.second);
   }
 
   return ans;
@@ -59,11 +69,9 @@ int main(){
 
   vector<int> res = topView(root); // TC and SC - O(N)
 
-  // for(auto it: res){
-  //   for(auto it2: it){
-  //     cout << it2 << " ";
-  //   }
-  // }
+    for(auto it2: res){
+      cout << it2 << " ";
+    }
 
   return 0;
 }
