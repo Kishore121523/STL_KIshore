704172c782b3371128edf1bd9b72c1057ec8c431
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-03-01 22:16:39 -0500

    Added Symmentry

---
+++ Trees/Symmentric.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node{
+  int data;
+  Node* left;
+  Node* right;
+
+  public:
+  Node(int data1){
+    data = data1;
+    left = right = NULL;
+  }
+};
+
+
+int main(){
+  return 0;
+}
