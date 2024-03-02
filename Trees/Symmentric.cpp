35b7e5cd105f8ca00dd07fceb9dc886b63cd51d7
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-03-01 23:42:02 -0500

    Edited Symmentry

--- Trees/Symmentric.cpp
+++ Trees/Symmentric.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 class Node{
+  public:
   int data;
   Node* left;
   Node* right;
@@ -12,9 +13,30 @@ class Node{
     data = data1;
     left = right = NULL;
   }
+
 };
+
+bool isSymmentricFoo(Node* left, Node* right){
+  if(left == NULL || right == NULL) return left==right;
+  if(left->data != right->data) return false;
+  return isSymmentricFoo(left->left, right->right) && isSymmentricFoo(left->right, right->left); 
+}
+
+bool isSymmentric(Node* root){
+  return root==NULL || isSymmentricFoo(root->left, root->right);
+}
 
 
 int main(){
+  Node* root = new Node(1);
+  root->left = new Node(2);
+  root->right = new Node(2);
+
+  root->left->left = new Node(3);
+  root->left->right = new Node(4);
+  root->right->left = new Node(4);
+  root->right->right = new Node(3);
+
+  cout << isSymmentric(root);
   return 0;
 }
