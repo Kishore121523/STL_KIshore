b3eb0b9a41bdc3217527e931c7e96ba0cbac3d1d
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-03-10 23:42:49 -0400

    Edited RootToNode

--- Trees/Hard/RootToNode.cpp
+++ Trees/Hard/RootToNode.cpp
@@ -20,11 +20,12 @@ bool getPath (Node* root, vector<int> &arr, int x){
   if(!root) return false;
 
   arr.push_back(root->data);
-  if(root->data == x) return true;
 
+  if(root->data == x) return true;
   if(getPath(root->left,arr,x) || getPath(root->right,arr,x)) return true;
   
   arr.pop_back();
+  
   return false;
 }
 
