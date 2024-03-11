c7871655e388ababe3807808bcbd65233b1aec66
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-03-10 23:23:27 -0400

    Edited RootToNode

---
+++ Trees/Hard/RootToNode.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node{
+  public:
+  int data;
+  Node* left;
+  Node* right;
+
+  public:
+  Node(int data1){
+    data = data1;
+    left = right = NULL;
+  }
+
+};
+
+bool getPath (Node* root, vector<int> &arr, int x){
+  if(!root) return false;
+
+  arr.push_back(root->data);
+  if(root->data == x) return true;
+
+  if(getPath(root->left,arr,x) || getPath(root->right,arr,x)) return true;
+  
+  arr.pop_back();
+  return false;
+}
+
+
+
+int main(){
+  Node* root = new Node(1);
+  root->left = new Node(2);
+  root->right = new Node(3);
+
+  root->left->left = new Node(4);
+  root->left->right = new Node(5);
+  root->right->left = new Node(6);
+  root->right->right = new Node(7);
+
+  int node = 4;
+  vector<int> arr;
+
+  getPath(root,arr,node); // TC and SC = O(N)
+
+  for(auto it: arr){
+      cout << it << " ";
+  }
+  return 0;
+}
